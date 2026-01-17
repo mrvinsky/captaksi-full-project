0ff3fbed5bdcbf08915911a30bf915168ba3e618
Besır Alkaya <mrvinsky@Besr-MacBook-Air.local>
2026-01-17 10:07:08 +0100

    feat: iOS Config, Knowledge Base Midnight UI, and Logo

--- captaksi_driver_app/ios/Runner/AppDelegate.swift
+++ captaksi_driver_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBh_TTuFpUAbM0yw3lrzq4PYTPBv_R9ivA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
